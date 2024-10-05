7595e0c5672110b1ccaf8a951c7d975cd86c3691
jaeminSon <woalsdnd@gmail.com>
2024-10-05 20:10:26 +0900

    add code for plot

---
+++ plot.py
@@ -0,0 +1,18 @@
+from utils import (
+    plot_kelly_2d,
+    plot_return_by_sector,
+    plot_return_index,
+    plot_portfolio,
+    plot_correlation,
+    plot_return_measure,
+    plot_covariance,
+)
+
+if __name__ == "__main__":
+    plot_kelly_2d()
+    plot_return_by_sector(start_date="2024-01-01")
+    plot_return_index(start_date="2024-01-01")
+    plot_portfolio(start_date="2024-01-01")
+    plot_correlation(start_date="2024-01-01", target="sectors")
+    plot_covariance(start_date="2024-01-01", target="sectors")
+    plot_return_measure(start_date="2024-01-01")
