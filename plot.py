1369839f0daddc2ca33fbff0fa7e1a8385ece5e8
jaeminSon <woalsdnd@gmail.com>
2024-10-06 18:20:42 +0900

    plot soaring stocks

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ from utils import (
     plot_correlation,
     plot_return_measure,
     plot_covariance,
+    plot_soaring_stocks,
 )
 
 if __name__ == "__main__":
@@ -18,3 +19,4 @@ if __name__ == "__main__":
     plot_correlation(start_date="2024-01-01", target="assets")
     plot_covariance(start_date="2024-01-01", target="assets")
     plot_return_measure(start_date="2024-01-01")
+    plot_soaring_stocks()
