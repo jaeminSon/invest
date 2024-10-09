d53cf7732ed06645b9c03680c790aba8843adbe5
jaeminSon <woalsdnd@gmail.com>
2024-10-09 17:06:37 +0900

    update figures

--- portfolio.py
+++ portfolio.py
@@ -1,12 +1,8 @@
 from utils import (
-    generate_portfolio_via_sampling,
-    generate_portfolio_via_efficient_frontier,
+    write_new_portfolio,
     compute_budge,
 )
 
 if __name__ == "__main__":
-    # generate_portfolio_via_sampling(start_date="2024-01-01", end_date="2024-08-30")
-    generate_portfolio_via_efficient_frontier(
-        start_date="2024-01-01", end_date="2024-08-30"
-    )
+    write_new_portfolio(rebalacing_period=10)
     # print(compute_budge(100_000))
