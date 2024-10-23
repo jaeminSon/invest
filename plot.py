58e70a90ff7b6603b1ab6492ccb1fd7a9a1ea07d
jaeminSon <woalsdnd@gmail.com>
2024-10-23 21:54:47 +0900

    update plot codes

--- plot.py
+++ plot.py
@@ -10,22 +10,22 @@ from utils import (
     plot_correlation,
     plot_soaring_stocks,
     plot_rebalancing_backtest,
+    plot_portfolio_via_sampling,
 )
 
 if __name__ == "__main__":
-    # plot_return_leverage_with_ma(start_date="2024-01-01")
-    # exit(1)
-    # plot_kelly_2d()
-    # plot_return_leverage(start_date="2020-01-01")
-    # plot_return_leverage_with_ma(start_date="2020-01-01")
-    # plot_return_by_sector(start_date="2000-01-01")
-    # plot_return_by_asset(start_date="2000-01-01")
-    # plot_return_index(start_date="2024-01-01")
-    # plot_return_portfolio_stocks(start_date="2024-01-01")
-    # plot_correlation(start_date="2024-01-01", target="sectors")
-    # plot_correlation(start_date="2024-01-01", target="assets")
-    # plot_return_measure(start_date="2024-01-01")
-    # plot_soaring_stocks()
+    plot_kelly_2d()
+    plot_portfolio_via_sampling("2012-01-01")
+    plot_return_leverage(start_date="2020-01-01")
+    plot_return_leverage_with_ma(start_date="2020-01-01")
+    plot_return_by_sector(start_date="2000-01-01")
+    plot_return_by_asset(start_date="2000-01-01")
+    plot_return_index(start_date="2024-01-01")
+    plot_return_portfolio_stocks(start_date="2024-01-01")
+    plot_correlation(start_date="2024-01-01", target="sectors")
+    plot_correlation(start_date="2012-01-01", target="assets")
+    plot_return_measure(start_date="2024-01-01")
+    plot_soaring_stocks()
 
     plot_rebalancing_backtest(
         start_date="2024-01-01",
@@ -33,22 +33,22 @@ if __name__ == "__main__":
         path_savefile="backtest_from_20240101.png",
     )
     plot_rebalancing_backtest(
-        start_date="2020-04-01",
-        end_date="2024-10-20",
-        path_savefile="backtest_from_20200401.png",
-    )
-    plot_rebalancing_backtest(
         start_date="2020-01-01",
-        end_date="2024-10-20",
+        end_date="2024-01-01",
         path_savefile="backtest_from_20200101.png",
     )
     plot_rebalancing_backtest(
-        start_date="2019-01-01",
-        end_date="2024-10-20",
-        path_savefile="backtest_from_20190101.png",
+        start_date="2016-01-01",
+        end_date="2020-01-01",
+        path_savefile="backtest_from_20160101.png",
     )
     plot_rebalancing_backtest(
-        start_date="2016-01-01",
+        start_date="2014-01-01",
+        end_date="2016-01-01",
+        path_savefile="backtest_from_20140101.png",
+    )
+    plot_rebalancing_backtest(
+        start_date="2014-01-01",
         end_date="2024-10-20",
-        path_savefile="backtest_from_20160101.png",
+        path_savefile="backtest_from_2014_to_2024.png",
     )
