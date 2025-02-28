5881901a4c939ca5c6e0063ca47da3766f9497fd
jaeminSon <woalsdnd@gmail.com>
2025-02-28 23:14:16 +0900

    update figures

--- plot.py
+++ plot.py
@@ -19,72 +19,79 @@ if __name__ == "__main__":
     ########################
     # for research purpose #
     ########################
-    plot_kelly("p_win", os.path.join("figures", "kelly_pwin.png"))
-    plot_kelly("profit", os.path.join("figures", "kelly_profit.png"))
-    plot_kelly("loss", os.path.join("figures", "kelly_loss.png"))
+    # plot_kelly("p_win", os.path.join("figures", "kelly_pwin.png"))
+    # plot_kelly("profit", os.path.join("figures", "kelly_profit.png"))
+    # plot_kelly("loss", os.path.join("figures", "kelly_loss.png"))
 
     ##################
     # plot quarterly #
     ##################
-    plot_nasdaq_divided_by_gdp(
-        start_date="1900-01-01",
-        path_savefile=os.path.join("figures", "nasdaq_over_gdp.png"),
-    )
-    plot_nasdaq_divided_by_m2(
-        start_date="1900-01-01",
-        path_savefile=os.path.join("figures", "nasdaq_over_m2.png"),
-    )
-    plot_sandp_divided_by_gdp(
-        start_date="1900-01-01",
-        path_savefile=os.path.join("figures", "sp500_over_gdp.png"),
-    )
-    plot_sandp_divided_by_m2(
-        start_date="1900-01-01",
-        path_savefile=os.path.join("figures", "sp500_over_m2.png"),
-    )
+    # plot_nasdaq_divided_by_gdp(
+    #     start_date="1900-01-01",
+    #     path_savefile=os.path.join("figures", "nasdaq_over_gdp.png"),
+    # )
+    # plot_nasdaq_divided_by_m2(
+    #     start_date="1900-01-01",
+    #     path_savefile=os.path.join("figures", "nasdaq_over_m2.png"),
+    # )
+    # plot_sandp_divided_by_gdp(
+    #     start_date="1900-01-01",
+    #     path_savefile=os.path.join("figures", "sp500_over_gdp.png"),
+    # )
+    # plot_sandp_divided_by_m2(
+    #     start_date="1900-01-01",
+    #     path_savefile=os.path.join("figures", "sp500_over_m2.png"),
+    # )
 
     ###################
     # plot every month #
     ###################
-    plot_SP500_JOLT(
-        start_date="1900-01-01", path_savefile=os.path.join("figures", "SP500_JOLT.png")
-    )
-    plot_correlation(
-        "sectors",
-        start_date="2024-01-01",
-        path_savefile=os.path.join("figures", "correlation_sectors.png"),
-    )
-    plot_sort_by_return(
-        "selected_etfs",
-        "2025-01-01",
-        path_savefile=os.path.join("figures", "sorted_etf.png"),
-    )
+    # plot_SP500_JOLT(
+    #     start_date="1900-01-01", path_savefile=os.path.join("figures", "SP500_JOLT.png")
+    # )
+    # plot_correlation(
+    #     "sectors",
+    #     start_date="2024-01-01",
+    #     path_savefile=os.path.join("figures", "correlation_sectors.png"),
+    # )
+    # plot_sort_by_return(
+    #     "selected_etfs",
+    #     "2025-01-01",
+    #     path_savefile=os.path.join("figures", "sorted_etf.png"),
+    # )
 
     ###################
     # plot every week #
     ###################
-    plot_return_by_group(
-        "sectors",
-        start_date="2025-01-01",
-        path_savefile=os.path.join("figures", "return_sectors.png"),
-    )
-    plot_return_by_group(
-        "index",
-        start_date="2025-01-01",
-        path_savefile=os.path.join("figures", "return_index.png"),
-    )
-    plot_return_by_group(
-        "x1_x3",
-        start_date="1900-01-01",
-        path_savefile=os.path.join("figures", "return_x1_x3.png"),
-    )
+    # plot_return_by_group(
+    #     "sectors",
+    #     start_date="2025-01-01",
+    #     path_savefile=os.path.join("figures", "return_sectors.png"),
+    # )
+    # plot_return_by_group(
+    #     "index",
+    #     start_date="2025-01-01",
+    #     path_savefile=os.path.join("figures", "return_index.png"),
+    # )
+    # plot_return_by_group(
+    #     "x1_x3",
+    #     start_date="1900-01-01",
+    #     path_savefile=os.path.join("figures", "return_x1_x3.png"),
+    # )
 
-    plot_return_with_ma(
-        "leverage",
-        start_date="1900-01-01",
-        path_savefile=os.path.join("figures", "return_leverage.png"),
-    )
+    ##############
+    # plot daily #
+    ##############
+    # plot_return_with_ma(
+    #     "leverage",
+    #     start_date="1900-01-01",
+    #     path_savefile=os.path.join("figures", "return_leverage.png"),
+    # )
 
     plot_density_function("leverage", start_date="1900-04-01", savedir="figures")
+    plot_density_function(
+        "sp500", start_date="1900-04-01", savedir="figures", domain=[0.6, 1.25]
+    )
 
-    plot_price_ratio("leverage", start_date="1900-04-01", savedir="figures")
+    # plot_price_ratio("leverage", start_date="1900-04-01", savedir="figures")
+    # plot_price_ratio("sp500", start_date="1900-04-01", savedir="figures")
